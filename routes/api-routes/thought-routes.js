9e4d5bc809e61aeb38355b52be670073c908e420
Parry <parryprogramming@gmail.com>
2024-05-28 21:48:10 -0400

    edited model though and user

--- routes/api-routes/thought-routes.js
+++ routes/api-routes/thought-routes.js
@@ -1,4 +1,3 @@
-// Import the necessary dependencies and controllers
 const router = require('express').Router();
 const {
     getAllThoughts,
@@ -10,16 +9,15 @@ const {
     deleteReaction,
 } = require('../../controllers/thought-controller');
 
-// Define the routes for GET and POST all Thoughts
 router.route('/').get(getAllThoughts).post(createThought);
 
-// Define the routes for GET, PUT and DELETE Thoughts
+
 router.route('/:thoughtId').get(getThoughtsById).put(updateThoughtById).delete(deleteThought);
 
-// Define the route for POST reaction to a Thought
+
 router.route('/:thoughtId/reactions').post(createReaction);
 
-// Define the route for DELETE reaction to a Thought
+
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
-// Export the router
+
 module.exports = router;
--- routes/api-routes/user-routes.js
+++ routes/api-routes/user-routes.js
@@ -1,4 +1,3 @@
-// Import the necessary dependencies and controllers
 const router = require('express').Router();
 const {
   getAllUsers,
@@ -8,16 +7,16 @@ const {
   deleteUserById,
   addFriend,
   removeFriend,
-//   checkFriendRemoved,
+
 } = require('../../controllers/user-controller');
 
-// GET and POST all users
+
 router.route('/').get(getAllUsers).post(createUser);
 
-// GET user id, PUT update user id and DELETE user by id
+
 router.route('/:userId').get(getUserById).put(updateUserById).delete(deleteUserById);
 
-// POST add friend and DELETE remove Friend
+
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
-// Export the router
+
 module.exports = router;
